d7992d20e027f8faa602d24349e3dea4c032a530
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-08-11 22:12:32 +0530

    If click on activate it will create option in admin menu

--- custom_plugin.php
+++ custom_plugin.php
@@ -1,3 +1,6 @@
+<?php
+
+
 /*
  * Plugin Name:       Custom Plugin 1
  * Plugin URI:        https://example.com/custom_plugin
@@ -8,3 +11,22 @@
  * Author:            Sujoy Sen
  * Author URI:        https://sujoysen.com
  */
+
+
+// Calling action hook to add menu 
+add_action("admin_menu", "cp_add_admin_menu");
+
+
+// Add Menu
+function cp_add_admin_menu() {
+    add_menu_page("Custom Plugin Menu", "Custom Plugin", "manage_options", "cp-plugin", "cp_handle_admin_menu", "dashicons-admin-home", 23);
+}
+
+
+// Menu handle callback
+function cp_handle_admin_menu() {
+    echo "<h2>Welcome to Custom Plugin Menu</h2>";
+}
+
+
+?>
