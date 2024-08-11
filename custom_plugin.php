b34ec22283c920f1a457e728d9e547fe86c1d311
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-08-11 19:04:30 +0530

    Create plugin basic structure

---
+++ custom_plugin.php
@@ -0,0 +1,10 @@
+/*
+ * Plugin Name:       Custom Plugin 1
+ * Plugin URI:        https://example.com/custom_plugin
+ * Description:       This is the custom plugin
+ * Version:           1.0
+ * Requires at least: 6.6.1
+ * Requires PHP:      8.1.6
+ * Author:            Sujoy Sen
+ * Author URI:        https://sujoysen.com
+ */
