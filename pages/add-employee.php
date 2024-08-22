16a0674e9e6971bd7923d2b00e1160ea0076a6e7
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-08-22 12:54:10 +0530

    View employee details

--- pages/add-employee.php
+++ pages/add-employee.php
@@ -2,7 +2,33 @@
 
   $message = "";
   $status = "";
+  $action = "";
+  $empId = "";
 
+  // Find request for view and edit
+  if(isset($_GET['action']) && isset($_GET['empId'])){
+
+    global $wpdb;
+    $empId = $_GET['empId'];
+
+    // Action: Edit
+    if($_GET['action'] == "edit"){
+      $action = "edit";
+    }
+
+    // Action: View
+    if($_GET['action'] == "view"){
+      $action = "view";
+    }
+
+    // Single employee informarion
+    $employee = $wpdb->get_row(
+      $wpdb->prepare("SELECT * FROM {$wpdb->prefix}ems_form_data WHERE id = '%d'", $empId), ARRAY_A
+    );
+
+  }
+
+  // Save form data
   if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["btn_submit"])){
 
     // Form Submitted
@@ -42,7 +68,15 @@
 
   <img src="<?php echo EMS_PLUGIN_URL ?>image/logo.jpg" style="width:100px;" />
 
-    <h2 class="sub-menu-head">Add Employee</h2>
+    <h2 class="sub-menu-head">
+      <?php 
+      if($action == "view"){
+        echo "View Employee";
+      }
+      else{
+        echo "Add Employee";
+      } ?>
+    </h2>
 
     <?php
     if(!empty($message)){
@@ -73,35 +107,75 @@
 
   <div class="form-group">
     <label for="name">Name:</label>
-    <input type="text" required class="form-control" id="name" name="name" aria-describedby="emailHelp" placeholder="Enter Your Name">
+    <input 
+    type="text" 
+    value="<?php if($action == 'view'){ echo $employee['name'];} ?>" 
+    required 
+    <?php if($action == "view"){ echo "readonly='readonly'"; } ?>
+    class="form-control" 
+    id="name" 
+    name="name" 
+    aria-describedby="emailHelp" 
+    placeholder="Enter Your Name">
   </div>
 
   <div class="form-group">
     <label for="email">Email:</label>
-    <input type="email" required class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter Your Email">
+    <input 
+    type="email" 
+    value="<?php if($action == 'view'){ echo $employee['email'];} ?>" 
+    required 
+    <?php if($action == "view"){ echo "readonly='readonly'"; } ?>
+    class="form-control" 
+    id="email" 
+    name="email" 
+    aria-describedby="emailHelp" 
+    placeholder="Enter Your Email">
   </div>
 
   <div class="form-group">
     <label for="phoneNo">Phone No.:</label>
-    <input type="number" class="form-control" id="phoneNo" name="phoneNo" aria-describedby="emailHelp" placeholder="Enter Your Phone Number">
+    <input 
+    type="number" 
+    value="<?php if($action == 'view'){ echo $employee['phoneNo'];} ?>" 
+    <?php if($action == "view"){ echo "readonly='readonly'"; } ?>
+    class="form-control" 
+    id="phoneNo" 
+    name="phoneNo" 
+    aria-describedby="emailHelp" 
+    placeholder="Enter Your Phone Number">
   </div>
 
   <div class="form-group">
     <label for="gender">Gender:</label>
-    <select class="form-control" id="gender" name="gender">
+    <select <?php if($action == "view"){echo "disabled";} ?> class="form-control" id="gender" name="gender">
       <option value="">Select Gender</option>
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-      <option value="other">Other</option>
+      <option value="male"<?php if($action == 'view' && $employee['gender'] == "male"){ echo "selected"; } ?>>Male</option>
+      <option value="female"<?php if($action == 'view' && $employee['gender'] == "female"){ echo "selected"; } ?>>Female</option>
+      <option value="other"<?php if($action == 'view' && $employee['gender'] == "other"){ echo "selected"; } ?>>Other</option>
     </select>
   </div>
 
   <div class="form-group">
     <label for="designation">Designation:</label>
-    <input type="text" class="form-control" id="designation" name="designation" aria-describedby="emailHelp" placeholder="Enter Your Designation">
+    <input 
+    type="text" 
+    value="<?php if($action == 'view'){ echo $employee['designation'];} ?>" 
+    <?php if($action == "view"){ echo "readonly='readonly'"; } ?>
+    class="form-control" 
+    id="designation" 
+    name="designation" 
+    aria-describedby="emailHelp" 
+    placeholder="Enter Your Designation">
   </div>
+  
+  <?php 
+      if($action == "view"){
 
-  <button type="submit" class="btn btn-primary" name="btn_submit">Submit</button>
+      }
+      else{
+        echo '<button type="submit" class="btn btn-primary" name="btn_submit">Submit</button>';
+      } ?>
 </form>
 
 </div>
