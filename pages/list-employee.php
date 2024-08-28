01ead52ccffacaf04798ca723ae9e2dee4630084
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-08-28 09:24:04 +0530

    Delete employee details

--- pages/list-employee.php
+++ pages/list-employee.php
@@ -2,14 +2,36 @@
 
 global $wpdb;
 
+
+
+// Delete Block
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(isset($_POST['emp_del_id']) && !empty($_POST['emp_del_id'])){
+        $wpdb->delete("{$wpdb->prefix}ems_form_data",array(
+            "id" => intval($_POST['emp_del_id'])
+        ));
+        $message = "Employee Deleted Successfully";
+    }
+}
+
 $employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_form_data", ARRAY_A);
 
 ?>
 
 <div class="container">
-  
-    <img src="<?php echo EMS_PLUGIN_URL ?>image/logo.jpg" style="width:100px;" />  
+
+    <img src="<?php echo EMS_PLUGIN_URL ?>image/logo.jpg" style="width:100px;" />
     <h2 class="sub-menu-head">List Employees</h2>
+
+    <?php
+        if(!empty($message)){
+            ?>
+            <div class="alert alert-success">
+                <?php echo $message; ?>
+            </div>
+            <?php
+        }
+    ?>
 
     <table class="table table-hover" id="tbl-employee">
         <thead>
@@ -29,20 +51,30 @@ $employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_form_data", AR
             if (count($employees) > 0) {
                 foreach ($employees as $employee) {
                     ?>
-                    <tr>
-                        <td><?php echo $employee['id']; ?></td>
-                        <td><?php echo $employee['name']; ?></td>
-                        <td><?php echo $employee['email']; ?></td>
-                        <td><?php echo $employee['phoneNo']; ?></td>
-                        <td><?php echo ucfirst($employee['gender']); ?></td>
-                        <td><?php echo $employee['designation']; ?></td>
-                        <td>
-                            <a href="admin.php?page=employee-system&action=edit&empId=<?php echo $employee['id']; ?>" class="btn btn-warning">Update</a>
-                            <a href="admin.php?page=employee-system&action=delete&empId=<?php echo $employee['id']; ?>" class="btn btn-danger">Delete</a>
-                            <a href="admin.php?page=employee-system&action=view&empId=<?php echo $employee['id']; ?>" class="btn btn-info">View</a>
-                        </td>
-                    </tr>
-                    <?php
+            <tr>
+                <td><?php echo $employee['id']; ?></td>
+                <td><?php echo $employee['name']; ?></td>
+                <td><?php echo $employee['email']; ?></td>
+                <td><?php echo $employee['phoneNo']; ?></td>
+                <td><?php echo ucfirst($employee['gender']); ?></td>
+                <td><?php echo $employee['designation']; ?></td>
+                <td>
+                    <a href="admin.php?page=employee-system&action=edit&empId=<?php echo $employee['id']; ?>"
+                        class="btn btn-warning">Update</a>
+
+                    <form id="frm-delete-employee-<?php echo $employee['id']; ?>" method="post"
+                        action="<?php echo $_SERVER['PHP_SELF'] ?>?page=list-employee">
+                        <input type="hidden" name="emp_del_id" value="<?php echo $employee['id']; ?>">
+                    </form>
+
+                    <a href="javascript:void(0)" onclick="if(confirm('Are you sure want to delete?')){
+                        jQuery('#frm-delete-employee-<?php echo $employee['id']; ?>').submit();
+                        }" class="btn btn-danger">Delete</a>
+                    <a href="admin.php?page=employee-system&action=view&empId=<?php echo $employee['id']; ?>"
+                        class="btn btn-info">View</a>
+                </td>
+            </tr>
+            <?php
                 }
             } else {
                 echo "<tr><td colspan='7'>No Employee Found</td></tr>";
